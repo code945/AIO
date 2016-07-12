09fe365c7bea2ea6f13e556c31f284a83fd31d81
code945 <lhx880619@163.com>
2016-07-12 18:38:14 +0800

    remove adminlte css google reference

--- backend/views/site/login.php
+++ backend/views/site/login.php
@@ -20,45 +20,17 @@ $fieldOptions2 = [
     'inputTemplate' => "{input}<span class='glyphicon glyphicon-lock form-control-feedback'></span>"
 ];
 $code = \common\helper\CommonHelper::guid();
-$js='
- 
-var ws = new WebSocket("ws://www.linhongxu.com:7272");
-ws.onopen = function(){
-	var init_data = \'{"type":"init","task_id":"'.$code.'"}\'; 
-    ws.send(init_data);
-};
-ws.onmessage = function(e){
-    console.log(e.data);
-    var data = eval("("+e.data+")");
-    switch(data[\'type\']) {
-            // 服务端ping客户端
-        case \'ping\':
-            ws.send(\'{"type":"pong"}\');
-            break;
-        case \'login_status\':
-            if(data["openid"] != "") 
-            location.href="/site/wechat-auth-handler?openid=" + data["openid"] ;
-            break;
-    }
-};
-
-
-$(document).ready(function () {
-    $("#qrimg").attr("src","'.Yii::getAlias('@authQRCodeUrl').'?auth_type=admin&task_id='.$code.'&target_back=http://admin.linhongxu.com/site/wechat-auth-handler"); 
-    });
-        ';
-$this->registerJs($js, View::POS_END);
 
 ?>
 
-<div class="login-box" style="width: 600px;">
+<div class="login-box" >
     <div class="login-logo">
-        <a href="#">系统后台</a>
+        <a href="#">后台管理系统</a>
     </div>
     <!-- /.login-logo -->
     <div class="login-box-body">
         <p class="login-box-msg">登陆后开始管理</p>
-        <div class="col-lg-6">
+        <div class="col-lg-12">
             <?php $form = ActiveForm::begin(['id' => 'login-form', 'enableClientValidation' => false]); ?>
 
             <?= $form
@@ -99,13 +71,6 @@ $this->registerJs($js, View::POS_END);
             </div>
 
             <?php ActiveForm::end(); ?>
-        </div>
-        <div class="col-lg-6">
-            <div class="text-center">
-                <img id="qrimg">
-                <p>- 微信扫码登陆-</p>
-            </div>
-            <!-- /.social-auth-links -->
         </div>
         <div style="clear: both"></div>
     </div>
