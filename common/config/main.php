e330c9f0d38523c179d9ca61cf0a2241fc4a47fc
code945 <lhx880619@163.com>
2016-07-20 13:19:04 +0800

    reset password

--- common/config/main.php
+++ common/config/main.php
@@ -1,6 +1,7 @@
 <?php
 return [
     'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
+    'name'=>'AIO',
     'aliases' => [
         '@frontendUrl' => 'http://www.linhongxu.com',
         '@backendUrl' => 'http://admin.linhongxu.com',
--- common/config/params.php
+++ common/config/params.php
@@ -1,7 +1,5 @@
 <?php
 return [
-    'adminEmail' => 'admin@example.com',
-    'supportEmail' => 'support@example.com',
     'defaultEmail' => 'lhx880619@163.com',
     'user.passwordResetTokenExpire' => 3600,
 ];
